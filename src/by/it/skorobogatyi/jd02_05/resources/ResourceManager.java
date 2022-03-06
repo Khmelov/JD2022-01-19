8a97b868ad745cf13b07c4f26ac0c1f8e7bdc03b
Maksim Skarabahaty <skaregok@gmail.com>
2022-03-06 19:02:16 +0300

    Skorobogatyi jd 02_05 A

---
+++ src/by/it/skorobogatyi/jd02_05/resources/ResourceManager.java
@@ -0,0 +1,27 @@
+package by.it.skorobogatyi.jd02_05.resources;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public enum ResourceManager {
+
+    INSTANCE;
+
+    public static final String BASE_NAME = "by.it.skorobogatyi.jd02_05.resources.language";
+
+    private ResourceBundle resourceBundle;
+
+
+    ResourceManager() {
+        Locale.setDefault(Locale.ENGLISH);
+        set(Locale.ENGLISH);
+    }
+
+    public String get(String key) {
+        return resourceBundle.getString(key);
+    }
+
+    public void set (Locale locale) {
+        resourceBundle = ResourceBundle.getBundle(BASE_NAME, locale);
+    }
+}
