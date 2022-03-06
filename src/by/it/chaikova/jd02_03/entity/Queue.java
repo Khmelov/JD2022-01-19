e3b19ffa3b2a0ca074f70f3ff7ac154bae2c99e3
Chaikova Anna <ekaterina-pirogova@ro.ru>
2022-03-06 18:11:00 +0300

    Task 02 - 03

---
+++ src/by/it/chaikova/jd02_03/entity/Queue.java
@@ -0,0 +1,34 @@
+package by.it.chaikova.jd02_03.entity;
+
+import by.it._classwork_.calculator.exceptions.ApplacitionException;
+import by.it.chaikova.jd02_03.utils.Constants;
+
+import java.util.Optional;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+
+public class Queue {
+    private final BlockingDeque<Customer> customerDeque = new LinkedBlockingDeque<>(
+            Constants.QUEUE_CUSTOMER_LENGTH
+    );
+    //private final Object monitor = new Object();
+
+
+    public void add(Customer customer) {
+       // synchronized (monitor) {
+
+            customerDeque.addLast(customer);
+       // }
+    }
+
+    public Optional<Customer> extact() {
+       // synchronized (monitor) {
+        try {
+            return Optional.ofNullable(customerDeque.pollFirst(1, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            throw new ApplacitionException(e);
+        }
+        // }
+    }
+}
