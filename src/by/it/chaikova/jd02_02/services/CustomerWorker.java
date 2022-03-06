5bbea06abba1098f95e98b93160facb1fafafdc9
Chaikova Anna <ekaterina-pirogova@ro.ru>
2022-03-06 12:42:05 +0300

    Task 02 - 02

---
+++ src/by/it/chaikova/jd02_02/services/CustomerWorker.java
@@ -0,0 +1,70 @@
+package by.it.chaikova.jd02_02.services;
+
+import by.it._classwork_.calculator.exceptions.ApplacitionException;
+import by.it.chaikova.jd02_02.entity.Customer;
+import by.it.chaikova.jd02_02.entity.Good;
+import by.it.chaikova.jd02_02.entity.Queue;
+import by.it.chaikova.jd02_02.utils.RandomData;
+import by.it.chaikova.jd02_02.utils.Sleeper;
+
+public class CustomerWorker extends Thread implements CustomerAction {
+
+    private final Customer customer;
+    private final Store store;
+
+    public CustomerWorker(Store store, Customer customer) {
+
+        this.customer = customer;
+        this.store = store;
+        this.setName("Worker for" + customer.toString() + " ");
+        store.getManager().customerIn();
+    }
+
+    @Override
+    public void run() {
+
+        enteredStore();
+        Good good = chooseGood();
+        System.out.println(customer + " choose" + good);
+        goOut();
+        store.getManager().customerGoOut();
+
+    }
+
+    @Override
+    public void enteredStore() {
+        System.out.println(customer + " entered to store ");
+    }
+
+    @Override
+    public Good chooseGood() {
+        System.out.println(customer + " started to choose goods ");
+        int timeOut = RandomData.get(500, 2000);
+        Sleeper.sleep(timeOut);
+        System.out.println(customer + " finished to choose good ");
+        return new Good();
+    }
+
+    @Override
+    public void goToQueue() {
+        System.out.println(customer + "waiting in Queue");
+        synchronized (customer) {
+            Queue queue = store.getQueue();
+            queue.add(customer);
+            customer.setWaiting(true);
+            while (customer.isWaiting()) {
+                try {
+                    customer.wait();
+                } catch (InterruptedException e) {
+                    throw new ApplacitionException(e);
+                }
+            }
+        }
+        System.out.println(customer + "left the Queue");
+    }
+
+    @Override
+    public void goOut() {
+        System.out.println(customer + " go out ");
+    }
+}
