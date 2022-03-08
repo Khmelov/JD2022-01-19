1bcdd50bdf2e22d6e85e17fdf11b2340f95ab009
galinaedyardovna <galinaedyardovnazaretskaya@gmail.com>
2022-03-08 22:05:29 +0300

    task 02_06

--- src/by/it/zaretskaya/jd02_05/TaskA.java
+++ src/by/it/zaretskaya/jd02_05/TaskA.java
@@ -1,10 +1,14 @@
 package by.it.zaretskaya.jd02_05;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class TaskA {
     public static void main(String[] args) {
-        ResourceBundle resourceBundle = ResourceBundle.getBundle("by.it.zaretskaya.jd02_05.resources.language");
+        Locale.setDefault(Locale.ENGLISH);
+        String baseName="by.it.zaretskaya.jd02_05.resources.language";
+        ResourceBundle resourceBundle = ResourceBundle.getBundle(baseName);
+        Locale locale = new Locale("be","BY");
         System.out.printf("%s\n%s\n%s %s %s\n",
         resourceBundle.getString("messages.welcome"),
         resourceBundle.getString("messages.question"),
