784c0b61b902ae8e3dc76255be88859a91a23c55
Palikk <7146514@gmail.com>
2022-03-08 21:13:30 +0300

    Module 2 Task 4A done

--- src/by/it/yushkevich/calculator/services/CalcService.java
+++ src/by/it/yushkevich/calculator/services/CalcService.java
@@ -6,6 +6,10 @@ import by.it.yushkevich.calculator.utils.Patterns;
 import by.it.yushkevich.calculator.repository.MapRepository;
 import by.it.yushkevich.calculator.model.Var;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,54 +17,93 @@ public class CalcService {
 
     private final VarRepository repository;
 
+    private final Map<String, Integer> priorityMap = Map.of(
+            "=", 0,
+            "+", 1,
+            "-", 1,
+            "*", 2,
+            "/", 2
+
+    );
+
+
     public CalcService(VarRepository repository) {
         this.repository = repository;
     }
 
     public Var calc(String expression) throws CalcException {
 
-        expression.replaceAll(Patterns.SPACES, "");
-        String[] parts = expression.split(Patterns.OPERATION, 2);// разделили не более чем на две части (т.е. нашли левую и правую часть)
-        if (parts.length == 1) {
-            return repository.create(expression);
+        expression = expression.replaceAll(Patterns.SPACES, "");
+        //A=2+3*5
+        //A 2 3 5
+        List<String> operands = new ArrayList<>(Arrays.asList(expression.split(Patterns.OPERATION)));
+        // = + *
+        List<String> operations = new ArrayList<>();
+        Matcher operationFinder = Pattern.compile(Patterns.OPERATION).matcher(expression);
+
+        while (operationFinder.find()) {
+            operations.add(operationFinder.group());
+        }
+
+        while (!operations.isEmpty()){
+            int index = getIndexOperation(operations);
+            String left = operands.remove(index);
+            String right = operands.remove(index);
+            String operation = operations.remove(index);
+
+            Var result = calcOneOperation(left, operation, right);
+            operands.add(index,result.toString());
         }
 
 
-        Var right = repository.create(parts[1]);
+            return repository.create(operands.get(0));
 
-        if (expression.contains("=")){
-            String name = parts[0];
+    }
+
+    private Var calcOneOperation(String leftStr, String operation, String rightStr) throws CalcException {
+
+        Var right = repository.create(rightStr);
+
+        if (operation.equals("=")) {
+            String name = leftStr;
             return repository.save(name, right);
 
         }
 
-        Var left = repository.create(parts[0]);
+        Var left = repository.create(leftStr);
 
         if (left == null || right == null) {
-            throw new CalcException(String.format("Incorrect expression " + expression));
+            throw new CalcException(String.format("Incorrect operation " + leftStr + operation + rightStr));
 
         }
 
-        Matcher matcher = Pattern.compile(Patterns.OPERATION).matcher(expression);
-        if (matcher.find()) {
 
-            String operation = matcher.group();
+        switch (operation) {
+            case "+":
+                return left.add(right);
+            case "-":
+                return left.sub(right);
+            case "*":
+                return left.mul(right);
+            case "/":
+                return left.div(right);
+        }
+            throw new CalcException("Wow");
+    }
 
-            switch (operation) {
-                case "+":
-                    return left.add(right);
-                case "-":
-                    return left.sub(right);
-                case "*":
-                    return left.mul(right);
-                case "/":
-                    return left.div(right);
+
+    private int getIndexOperation(List<String> operations) {
+        int index = -1;
+        int currentPriority = -1;
+
+        for (int i = 0; i < operations.size(); i++) {
+            String op = operations.get(i);
+            if (currentPriority < priorityMap.get(op)) {
+                currentPriority = priorityMap.get(op);
+                index = i;
             }
 
         }
-
-
-       throw new CalcException("Something wen wrong");
-
+        return index;
     }
 }
