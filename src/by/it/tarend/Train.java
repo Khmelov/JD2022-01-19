915087830a51c7395959058bd09bc709f228bc6f
MaximTarend <dedmasim@gmail.com>
2022-03-08 05:28:07 +0300

    Tarend module02 4 ABC, 5 AB

--- src/by/it/tarend/Train.java
+++ src/by/it/tarend/Train.java
@@ -1,39 +1,19 @@
 package by.it.tarend;
 
-import by.it.tarend.calc.exceptions.CalcException;
-import by.it.tarend.calc.services.CalcService;
-import by.it.tarend.calc.utils.Patterns;
 
-import java.util.Stack;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class Train {
-    public static void main(String[] args) throws CalcException {
-        // D=((C-0.15)-20)/(7-5)
-//       String expression = "D=((C-0.15)-20)/(7-5)";
+    public static void main(String[] args) {
 
-        String expression = "C=B+(A*2)";
+        ArrayList<Integer> ar = new ArrayList<Integer>();
+        List temp = ar;
+        temp.add(new java.util.Date());
 
-
-        int braceCounter = 0;
-        int openBrace = expression.indexOf('(');
-        int closeBrace = 0;
-        for (int i = openBrace; i < expression.length(); i++) {
-            if (expression.charAt(i) == '(') {
-                braceCounter++;
-            }
-            if (expression.charAt(i) == ')') {
-                braceCounter--;
-            }
-            if (braceCounter == 0) {
-                closeBrace = i;
-                break;
-            }
-        }
-        String subExpression = expression.substring(openBrace + 1, closeBrace);
-        System.out.println(subExpression);
-        expression = expression.replace("("+subExpression+")", "77");
-
-        System.out.println(expression);
+        System.out.println(ar.get(0));
 
     }
 }
