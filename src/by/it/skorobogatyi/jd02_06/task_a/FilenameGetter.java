75adbe44fd6bf1a694c9a05b80886d9607b41e1b
Maksim Skarabahaty <skaregok@gmail.com>
2022-03-08 12:33:28 +0300

    Skorobogatyi jd 02_06 A

---
+++ src/by/it/skorobogatyi/jd02_06/task_a/FilenameGetter.java
@@ -0,0 +1,19 @@
+package by.it.skorobogatyi.jd02_06.task_a;
+
+import static java.io.File.separator;
+
+
+public class FilenameGetter {
+
+    private static final String DOT = ".";
+    public static final String USER_DIR = "user.dir";
+
+
+    public static String getFilename(Class<?> aClass, String root, String filename) {
+        String userDir = System.getProperty(USER_DIR);
+        String fullRootDir = userDir + separator + root + separator;
+        String returnString = aClass.getPackageName().replace(DOT, separator);
+        filename = returnString + separator + filename;
+        return fullRootDir + filename;
+    }
+}
---
+++ src/by/it/skorobogatyi/jd02_06/task_a/Log.java
@@ -0,0 +1,8 @@
+package by.it.skorobogatyi.jd02_06.task_a;
+
+public interface Log {
+
+    void error(String message);
+
+    void info(String message);
+}
