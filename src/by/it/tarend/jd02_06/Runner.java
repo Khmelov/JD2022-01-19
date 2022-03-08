d1312e4bfc37200315a17b7a10bbe3a0f17584b5
MaximTarend <dedmasim@gmail.com>
2022-03-08 20:04:29 +0300

    Tarend module02 4 ABC, 5 AB

---
+++ src/by/it/tarend/jd02_06/Runner.java
@@ -0,0 +1,21 @@
+package by.it.tarend.jd02_06;
+
+public class Runner {
+    public static void main(String[] args) {
+        Logger.getInstance().info("Start logging");
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread("thread № " + i + " ") {
+                @Override
+                public void run() {
+                    Logger logger = Logger.getInstance();
+                    logger.error(this.getName() + "Wrong");
+                    logger.info(this.getName() + "is OK");
+                }
+
+            };
+            thread.start();
+        }
+
+    }
+
+}
